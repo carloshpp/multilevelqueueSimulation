359aa5f6f14f4db28e78ddb3349ec0335edc37ff
carloshpp <kaike_henrique_godim@hotmail.com>
2014-05-06 09:32:35 -0300

    Merge origin/master

--- MultilevelQueueSimulation/src/multilevelqueuesimulation/Process.java
+++ MultilevelQueueSimulation/src/multilevelqueuesimulation/Process.java
@@ -1,25 +1,23 @@
-
 package multilevelqueuesimulation;
 
-
 public class Process {
-    
-    
+
     private int PID;
     private int Burst;
-    private int Turnaround;
-    private int WaitTime;
+    private float Turnaround;
+    private float WaitTime;
+    private float CreatedTime;
 
-    public Process(int pid, int burst, int turnaround, int waitTime){
-        
+    public Process(int pid, int burst, float turnaround, float waitTime, float createdTime) {
+
         this.PID = pid;
         this.Burst = burst;
         this.Turnaround = turnaround;
         this.WaitTime = waitTime;
+        this.CreatedTime = createdTime;
+        
     }
-    
-    
-    
+
     public int getPID() {
         return PID;
     }
@@ -32,12 +30,11 @@ public class Process {
         return Burst;
     }
 
-  
     public void setBurst(int Burst) {
         this.Burst = Burst;
     }
 
-    public int getTurnaround() {
+    public float getTurnaround() {
         return Turnaround;
     }
 
@@ -45,16 +42,26 @@ public class Process {
         this.Turnaround = Turnaround;
     }
 
-  
-    public int getWaitTime() {
+    public float getWaitTime() {
         return WaitTime;
     }
 
     public void setWaitTime(int WaitTime) {
         this.WaitTime = WaitTime;
     }
-    
-    
-    
-    
+
+    /**
+     * @return the CreatedTime
+     */
+    public float getCreatedTime() {
+        return CreatedTime;
+    }
+
+    /**
+     * @param CreatedTime the CreatedTime to set
+     */
+    public void setCreatedTime(int CreatedTime) {
+        this.CreatedTime = CreatedTime;
+    }
+
 }
